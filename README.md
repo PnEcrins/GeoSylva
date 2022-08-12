78b4a04fc1be90aa14fcd1d04ceda0b536f72f3c
ThomaSigma <tpauti2108@gmail.com>
2022-08-12 11:34:52 +0200

    ajout non url js

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # GeoSylva
+
 Visualisateur cartographique des patrimoines forestiers du Parc national des Ecrins
 
+## Instalation
+
+Définir une variable d'environnement `URL_GEOSERVER`
+
+    export URL_GEOSERVER=http://mon_url.io
--- johnscript.py
+++ johnscript.py
@@ -1,15 +1,13 @@
+import os
 import jinja2
 
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
 
 
-TEMPLATE_FILE = "geosylva.xml"
+TEMPLATE_FILE = "geosylva.xml.template"
 template = templateEnv.get_template(TEMPLATE_FILE)
-outputText = template.render(
-url_geoserver="http://188.165.118.84/geoserver/geoserver/cite/wms"
-) # this is where to put args to the template renderer
+outputText = template.render(url_geoserver=os.environ["URL_GEOSERVER"])
 
-print(outputText)
 with open("geosylvax.xml", "w") as f:
     f.write(outputText)
